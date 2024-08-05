ee7e04972c5e991bd9cff4409d887b6effec49d0
Coimbra <adsjkbd@gmail.com>
2024-08-05 07:35:47 -0300

    Preparação do projeto

---
+++ src/DbException.java
@@ -0,0 +1,10 @@
+import java.io.Serial;
+
+public class DbException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public DbException(String msg) {
+        super(msg);
+    }
+}
