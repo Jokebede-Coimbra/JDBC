e5cd65704c3339ede7c469a57af7b1fac0881ac7
Coimbra <adsjkbd@gmail.com>
2024-08-07 07:17:12 -0300

    Demo - inserir dados

---
+++ src/application/Program2.java
@@ -0,0 +1,34 @@
+package application;
+
+import db.DB;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Program2 {
+    public static void main(String[] args) {
+
+        Connection connection = null;
+        Statement st = null; // comando sql
+        ResultSet rs = null;
+        try {
+            connection = DB.getConnection();
+
+            st = connection.createStatement();
+
+            rs = st.executeQuery("select * from department");
+
+            while (rs.next()) {
+                System.out.println(rs.getInt("Id") + ", " + rs.getString("name"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            DB.closeResultSet(rs);
+            DB.closeStatement(st);
+            DB.closeConnection();
+        }
+    }
+}
