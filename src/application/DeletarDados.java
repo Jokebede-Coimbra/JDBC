666a217b80225ffea77f834c6ddc74d4d9eb423c
Coimbra <adsjkbd@gmail.com>
2024-08-09 05:58:43 -0300

    Demo - deletar dados

---
+++ src/application/DeletarDados.java
@@ -0,0 +1,36 @@
+package application;
+
+import db.DB;
+import db.DbIntegrityException;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class DeletarDados {
+    public static void main(String[] args) {
+
+        Connection connection = null;
+        PreparedStatement st = null;
+
+        try {
+            connection = DB.getConnection();
+
+            st = connection.prepareStatement("" +
+                    "DELETE FROM department "
+                    + "WHERE "
+                    + "Id = ? ");
+
+            st.setInt(1, 2);
+
+            int rowsAffected = st.executeUpdate();
+
+            System.out.println("Done!, Rows affected: " + rowsAffected);
+        } catch (SQLException e) {
+            throw new DbIntegrityException(e.getMessage());
+        } finally {
+            DB.closeConnection();
+            DB.closeStatement(st);
+        }
+    }
+}
